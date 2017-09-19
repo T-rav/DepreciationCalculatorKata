61e056e95ad4a6671e49ae9a4068f7fcdc4b1f13
T-rav <tmfrisinger@gmail.com>
2017-09-19 11:18:43 +0200

    First 2 test

---
+++ source/Kata/DepreciationCalculatorTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Depreciation.Calculator.Tests
+{
+    [TestFixture]
+    public class DepreciationCalculatorTests
+    {
+        [TestFixture]
+        public class UsefulLifeOneYear_AssetOwnedForFullYear
+        {
+
+            [Test]
+            public void GetStraightLineAmount_WhenNoSalvageValue_ShouldReturnAssetCost()
+            {
+                //---------------Arrange-------------------
+                var expected = 100;
+                var assetCost = 100;
+                var salvageValue = 0;
+                var calculator = new DepreciationCalculator();
+                //---------------Act----------------------
+                var result = calculator.GetStraightLineAmount(assetCost, salvageValue);
+                //---------------Assert-----------------------
+                Assert.AreEqual(expected, result);
+            }
+
+            [Test]
+            public void GetStraightLineAmount_WhenR25SalvageValue_ShouldReturnAssetCostMinusSalvageValue()
+            {
+                //---------------Arrange-------------------
+                var expected = 75;
+                var assetCost = 100;
+                var salvageValue = 25;
+                var calculator = new DepreciationCalculator();
+                //---------------Act----------------------
+                var result = calculator.GetStraightLineAmount(assetCost, salvageValue);
+                //---------------Assert-----------------------
+                Assert.AreEqual(expected, result);
+            }
+        }
+    }
+
+    public class DepreciationCalculator
+    {
+        public double GetStraightLineAmount(int assetCost, int salvageValue)
+        {
+            return assetCost - salvageValue;
+        }
+    }
+}
